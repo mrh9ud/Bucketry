6b87195ebf9cd536a77d85509b67cc22b489302b
singingcactus <bechma02@gmail.com>
2019-12-04 00:56:59 -0500

    full CRUD functionality on story branch

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,5 @@
 class StoriesController < ApplicationController
-    before_action :get_story, only: [:show, :edit, :update]
+    before_action :get_story, only: [:show, :edit, :update, :destroy]
 
     def index
         if params[:blog_expand]
@@ -14,10 +14,47 @@ class StoriesController < ApplicationController
     def show
     end
 
+    def new
+        @story = Story.new()
+    end
+
+    def create
+        @story = Story.create(strong_params)
+
+        if @story.valid?
+            redirect_to story_path(@story)
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        @story.update(strong_params)
+
+        if @story.valid?
+            redirect_to story_path(@story)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        user_id = @story.user.id
+        @story.destroy
+        redirect_to user_path(@story.user)
+    end
+
 
     private
 
     def get_story
         @story = Story.find(params[:id])
+    end
+
+    def strong_params
+        params.require(:story).permit(:title, :content, :user_experience_id)
     end
 end
