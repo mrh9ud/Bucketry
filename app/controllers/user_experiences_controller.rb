7b7cf4dfe32dfb4289dfbd2d78161ffdbbe38951
singingcactus <bechma02@gmail.com>
2019-12-04 18:16:50 -0500

    toggle button added

--- app/controllers/user_experiences_controller.rb
+++ app/controllers/user_experiences_controller.rb
@@ -1,4 +1,5 @@
 class UserExperiencesController < ApplicationController
+    before_action :get_user_experience, only: [:update, :destroy]
     before_action :require_login, only: [:new, :create, :edit, :update, :destroy]
     
     def new
@@ -15,7 +16,22 @@ class UserExperiencesController < ApplicationController
         end
     end
 
+    def update 
+        byebug
+        @user_experience.update(user_experience_params)
+
+        if @user_experience.valid?
+            redirect_to user_path(@user_experience.user)
+        else
+            render :edit
+        end
+    end
+
     private
+
+    def get_user_experience
+        @user_experience = UserExperience.find(params[:id])
+    end
 
     def user_experience_params
         params.require(:user_experience).permit(:user_id, :activity_id, :completed, :completion_date, :goal_date, activity_attributes: [:name, :description])
