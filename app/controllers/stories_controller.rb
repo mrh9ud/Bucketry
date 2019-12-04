c0626cbdf375188042b86d110eb4d1649e2d684d
singingcactus <bechma02@gmail.com>
2019-12-03 22:40:51 -0500

    index page functioning

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,23 @@
 class StoriesController < ApplicationController
+    before_action :get_story, only: [:show, :edit, :update]
+
+    def index
+        if params[:blog_expand]
+            session[:blog_length] += 5
+        else
+            session[:blog_length] = 5
+        end
+        
+        @stories = Story.order(created_at: :desc).limit(session[:blog_length]).reverse
+    end
+
+    def show
+    end
+
+
+    private
+
+    def get_story
+        @story = Story.find(params[:id])
+    end
 end
