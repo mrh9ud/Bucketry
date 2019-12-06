312d31f50516d3a8d64078c6c3916dab84931657
singingcactus <bechma02@gmail.com>
2019-12-06 14:22:04 -0500

    toggled case sensitivity on category validator

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
     has_many :category_activities
     has_many :activities, through: :category_activities
     has_many :user_experiences, through: :activities
-    validates :name, {presence: true, uniqueness: {case_sensitive: true}}
+    validates :name, {presence: true, uniqueness: {case_sensitive: false}}
 end
